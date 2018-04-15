74c9fc14e35abcae01019256b7f52089f3cbede2
AwayQuEM <awayquym@gmail.com>
2018-04-15 22:57:28 +0800

    TODO:不同的箭头

--- src/uml/class-diagram-relationship.js
+++ src/uml/class-diagram-relationship.js
@@ -13,34 +13,67 @@
 import {ClassDiagram} from "./class-diagram";
 
 
+var g = new dagreD3.graphlib.Graph().setGraph({});
+
+// ["normal", "vee", "undirected"].forEach(function(arrowhead) {
+//     g.setNode(arrowhead + "1", { label: " " });
+//     g.setNode(arrowhead + "2", { label: " " });
+//     g.setEdge(arrowhead + "1", arrowhead + "2", {
+//         arrowhead: arrowhead,
+//         label: arrowhead
+//     });
+// });
+
+
 // "normal", "vee", "undirected"
 
-function Relationship() {
-
-}
+// function Relationship() {
+//
+// }
 
 
 
 function Generalization() {
-
+    return {
+        arrowhead: "normal",
+    }
 }
 
 function Realization() {
-
+    return {
+        arrowhead: "normal",
+    }
 }
 
 function Association() {
-
+    return {
+        arrowhead: "normal",
+    }
 }
 
 function Aggregation() {
-
+    return {
+        arrowhead: "normal",
+    }
 }
 
 function Composition() {
-
+    return {
+        arrowhead: "normal",
+    }
 }
 
 function Dependency() {
+    return {
+        arrowhead: "normal",
+    }
+}
 
+export {
+    Generalization,
+    Realization,
+    Association,
+    Aggregation,
+    Composition,
+    Dependency,
 }
